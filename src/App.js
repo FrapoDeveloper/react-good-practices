646e0907fcf62bf3a3e73a644d6330b9209ccff1
Fraporitmos <fran_espino189@outlook.com>
2020-10-28 17:55:50 -0500

    2do commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import './styles/styles.scss';
-import Cardcourse from './components/cardcourse'
+import './styles/style.css';
+import Animation from './components/animation.jsx'
+import Cardcourse from './components/cardcourse';
+
+const cursos = [
+{
+  key :1,
+  title :"Curso de OpenGL",
+  img :  "https://i.ytimg.com/vi/v7d3ic_lmGw/maxresdefault.jpg",
+  price : "65"
+},{
+  key :2,
+  title :"Java Spring",
+  img :  "https://edteam-media.s3.amazonaws.com/courses/medium/6cbc514c-1e04-4bf4-8beb-fa595f363d9d.png",
+  price : "48"
+},{
+  key :3,
+  title :"Curso de Go",
+  img :  "https://edteam-media.s3.amazonaws.com/courses/big/e737f2e3-92b3-400e-8d82-1b19e0277012.png",
+  price : "52"
+},{
+   key :4,
+  title :"App PWA",
+  img :  "https://edteam-media.s3.amazonaws.com/courses/big/084a20f8-e327-47ce-a073-1df1f90e4a74.jpg",
+  price : "28"
+},{
+   key :5,
+  title :"Ayas y JS",
+  img :  "https://edteam-media.s3.amazonaws.com/courses/big/358b2db1-a529-4892-92a6-23bf97092c32.jpg",
+  price : "63"
+}
+]
+
 function App() {
   return (
     <>
@@ -8,33 +40,40 @@ function App() {
           <div className="ed-grid lg-grid-2 row-gap s-gap-2 m-gap-4">
             <div className="s-column s-main-center lg-main-start lg-cross-start s-center lg-left">
                   <h1 className="bigger-title">
-                      <span className="color white-color">Banner principal 
-                      <span className="color blue-400">de dos columnas</span>
-                      con EDgrid</span>
+                      <span className="color white-color">Hola, bienvenid@ <br/> 
+                      <span className="color blue-400">Este es mi portafolio</span>
+                    </span>
                   </h1>
-                  <p className="white-color s-opacity-8">Creado utilizando los componentes de columnas. Totalmente responsive.</p>
+                  <p className="white-color s-opacity-8">¡Exposicion de tecnologias informaticas con vision empresarial!</p>
                   <div className="s-main-center">
-                      <a className="button blue-500 s-mr-2 s-mb-2 m-mb-0">Botón 1</a>
-                      <a className="button ghost blue-400 s-mb-2 m-mb-0">Botón 2</a>
+                      <p className="button blue-500 s-mr-2 s-mb-2 m-mb-0">Botón 1</p>
+                      <p  className="button ghost blue-400 s-mb-2 m-mb-0">Botón 2</p>
                   </div>
             </div>
                   <div>
                       <div className="img-container s-ratio-16-9">
-                          <img src="https://ed-grid.com/assets/img/og-image.jpg" className="s-radius-1" />
+                        <Animation/>
                       </div>
                   </div>
             </div>
         </section>
 
-        <div className="ed-grid m-grid-3">
-        <Cardcourse />
-        <Cardcourse />
-        <Cardcourse />
-        <Cardcourse />
+        <div className="ed-grid m-grid-3 center">
+        {
+          cursos.map(({key,img,title,price})=>(
+            <Cardcourse
+            key={key}
+            img={img}
+            title={title}
+            price={price}
+            />
+          ) )
+
+//          cursos.map(c => <Cardcourse key= {c.key} title = {c.title} img={c.img} price={c.price} />)
+        }
 
         </div>
-        
-
+    
   </>
   );
 }
--- src/components/cardcourse.jsx
+++ src/components/cardcourse.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 
-const Cardcourse = () => (
+
+const Cardcourse = ({title,img,price}) => (
     <>
-        <article className="s-shadow-bottom">
+        <article className="card">
             <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
-                <img src="https://ed-grid.com/assets/img/poster-curso.png" />
+                <img src={img} alt={title}/>
             </div>
 
             <div className="s-bg-white s-pxy-2">
-                <h3>Curso Bases de Datos Desde Cero</h3>
-                <p className="s-mb-0">Diseña, estructura y administra bases de datos SQL y crea un sistema de facturación</p>
+                <h3>{title}</h3>
             </div>
-            <footer className="s-cross-center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
-
-                <div className="s-10 s-mr-1">
-
-                <div className="circle ">
-                    <img  src="https://ed-grid.com/assets/img/alexys.jpg"/>
-                </div>
-
-                </div>
-                <p className="s-mb-0">Prof. Alexys Lozada</p>
-                <div className="button s-to-right">$40USD</div>
-            </footer>
+       
+            <div className="s-main-center s-pxy-2">
+                <span  className="button s-to-center ">{`S/ ${price} soles`}</span>
+            </div>
         </article>
     </>
 
 )
+
 export default Cardcourse;
